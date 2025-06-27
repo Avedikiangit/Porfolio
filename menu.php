393ec5ffcfcdfa46b84d94d5d6eaab053792d17a
Avedikiangit <paqui22379@gmail.com>
2025-06-27 12:00:05 -0300

    cambios

---
+++ menu.php
@@ -0,0 +1,29 @@
+ <nav class="navbar navbar-expand-md ">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+          aria-controls="navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbar-collapse">
+          <ul class="navbar-nav me-auto mb-2 mb-md-0 barrainicio">
+            <li class="nav-item">
+              <a class="nav-link " href="index.html">Inicio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active px-3" href="contacto.html">Contacto</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="sobre-mi.html">Sobre mi</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="proyectos.html">Proyectos</a>
+            </li>
+        </div>
+
+        <div class="CV">
+          <a href="">Descargar mi CV<i class="fas fa-download"></i></a>
+
+        </div>
+      </div>
+    </nav>
